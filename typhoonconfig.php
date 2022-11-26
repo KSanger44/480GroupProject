499923f9099d5c4efc67699512aa96191b7d771c
Kyle Sanger <ksanger@edgewood.edu>
2022-11-26 16:13:27 -0600

    config connect update

--- typhoonconfig.php
+++ typhoonconfig.php
@@ -1,7 +1,15 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'typhoon');
-   $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+         //connect to the db schema
+         $servername = "localhost:3306";
+         $username = "root";
+         $password = '';
+         $dbname = "typhoon";
+   
+         // Create connection
+         $conn = new mysqli($servername, $username, $password, $dbname);
+   
+            // Check connection
+         if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+      }
 ?>
